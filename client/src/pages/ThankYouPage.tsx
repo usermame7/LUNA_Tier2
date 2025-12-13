d67cd090cfb5204e501c71df4b742c3f564f8fb4
usermame7 <154531526+usermame7@users.noreply.github.com>
2025-12-13 15:20:58 +0200

    Update Telegram link to +U7Lfj0uwoPQ1ZGFk

--- client/src/pages/ThankYouPage.tsx
+++ client/src/pages/ThankYouPage.tsx
@@ -3,7 +3,7 @@ import { Button } from "@/components/ui/button";
 import { CheckCircle } from "lucide-react";
 
 export default function ThankYouPage() {
-  const telegramUrl = "https://t.me/+JR-RXRoIm8o1YWRk";
+  const telegramUrl = "https://t.me/+U7Lfj0uwoPQ1ZGFk";
 
   useEffect(() => {
     // Google Analytics tracking only - NO Lead event here
